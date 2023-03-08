e53ffeb2b31daaf52f30fe1776c360ac93bc2570
Renan Augusto Mendes Carlos <114965499+RenanAUG@users.noreply.github.com>
2023-03-08 19:28:06 -0300

    Add files via upload

---
+++ Cachorro/src/main/java/nomeesexo/cachorro/TesteCachorro.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nomeesexo.cachorro;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteCachorro {
+    
+    public static void main(String[] args){
+     
+        Cachorro teste= new Cachorro();
+         teste.altura = 1.20;
+         teste.cor = "azul";
+         teste.idade = 5;
+         teste.peso = 10.5;
+         teste.qtpatas = 3;
+         
+        Cachorro teste1 = new Cachorro();
+         teste1.altura = 1.00;
+         teste1.cor = "amarelo";
+         teste1.idade = 7;
+         teste1.peso = 15.0;
+         teste1.qtpatas = 4;
+         
+        Cachorro teste2 = new Cachorro();
+         teste2.altura = 2.00;
+         teste2.cor = "preto";
+         teste2.idade = 4;
+         teste2.peso = 9.0;
+         teste2.qtpatas = 4;
+         
+        Cachorro teste3 = new Cachorro();
+         teste3.altura = 0.70;
+         teste3.cor = "marrom";
+         teste3.idade = 2;
+         teste3.peso = 2.0;
+         teste3.qtpatas = 3;
+                  
+        Cachorro teste4 = new Cachorro();
+         teste4.altura = 1.00;
+         teste4.cor = "branco";
+         teste4.idade = 9;
+         teste4.peso = 12.0;
+         teste4.qtpatas = 2;
+    
+//         construtor é o método que inicia o código fonte, ele unifica e organiza.
+    
+    }    
+}
